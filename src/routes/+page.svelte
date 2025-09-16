66345668b82dc2a5b98010bb523b4eff3f031fe8
Alex Nicol <nicol.alexandre1@gmail.com>
2025-09-16 13:26:03 +0000

    feat: base site

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<section>
+    <h1>&#128075; Hi, I'm <span>Alex Nicol!</span></h1>
+    <h2>I'm a <span>Software Engineer.</span></h2>
+</section>
+<section>
+    <p>I am passionate about the web, and what it provides and enables for users around the world.</p>
+    <p>Over the past 10 years, I have been lucky to work in many different industries, from education to energy, and with multiple technologies, such as Typescript, React, Svelte, Elm, Ruby on Rails, Node.js, Python and many more.</p>
+    <p>Nowadays, my focus is on building fast and accessible websites, for everyday people.</p>
+    <p>I believe in a free and open web, where information and knowledge is accessible to all.</p>
+    <p>I am currently working at Kraken Technologies as a Lead Software Engineer, where I lead the team building consumer facing websites for our water clients.</p>
+    <p>Previously, I was a Staff Software Engineer at BridgeU, where I led the frontend chapter and oversaw the frontend transformation of our student facing portal.</p>
+    <p>My <a href="https://www.linkedin.com/in/alexnicol/" target="_blank">LinkedIn profile</a> is the best place to see the rest of past experiences.</p>
+    <p>I've had the privilege to give talks at conferences and meetups, mostly about <a href="https://www.youtube.com/watch?v=vcVttSm3e8k" target="_blank">conversational systems</a> and <a href="https://www.youtube.com/watch?v=uJwvY0K6TkY" target="_blank">accessibility</a>.</p>
+    <p>Feel free to reach out at: <a href="mailto:alex@alexnicol.dev" target="_blank">alex@alexnicol.dev</a></p>
+    <p>When I'm not coding, most of my time is spent looking after my 2 daughters and my dog. I also enjoy bouldering, skateboarding, and playing video games.</p>
+</section>
+
+<style>
+    h1 > span, h2 > span {
+        color: var(--pico-primary);
+    }
+</style>
